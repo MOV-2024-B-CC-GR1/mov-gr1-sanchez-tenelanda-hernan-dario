aaac0e488bd2277925c08e3440bd67bf1a929941
Hernán Sánchez <105751698+HdSanch@users.noreply.github.com>
2024-11-27 18:23:29 -0500

    Update Main.kt

--- 00-Kotlin/Kotlin-Ejemplo/src/main/kotlin/Main.kt
+++ 00-Kotlin/Kotlin-Ejemplo/src/main/kotlin/Main.kt
@@ -51,12 +51,101 @@ fun main(args: Array<String>) {
     calcularSueldo(10.00, bonoEspecial = 20.00) // usando el parametro bonoEspecial en 2da posicion
     calcularSueldo(bonoEspecial = 20.00, sueldo=10.00, tasa=14.00)
 
+    val sumaA = Suma(1,1)
+    val sumaB = Suma(null, 1)
+    val sumaC = Suma(1, null)
+    val sumaD = Suma(null, null)
+    sumaA.sumar()
+    sumaB.sumar()
+    sumaC.sumar()
+    sumaD.sumar()
+    println(Suma.pi)
+    println(Suma.elevarAlCuadrado(2))
+    println(Suma.historialSumas)
+
 
     // usando el parametro bonoEspecial en 1ra posicion
     // usando el parametro sueldo en 2da posicion
     // usando el parametro tasa en 3era posicion
     // gracias a los parametros nombrados
 
+    val arregloEstatico: Array<Int> = arrayOf<Int>(1,2,3)
+    println(arregloEstatico);
+    val arregloDinamico: ArrayList<Int> = arrayListOf<Int>(
+        1,2,3,4,5,6,7,8,9,10
+    )
+    println(arregloDinamico)
+    arregloDinamico.add(11)
+    arregloDinamico.add(12)
+    println(arregloDinamico)
+
+    // FOR EACH = > Unit
+    // Iterar un arreglo
+
+
+    val respuestaForEach: Unit = arregloDinamico
+        .forEach { valorActual: Int -> //  - > = >
+            println("Valor actual: ${valorActual}");
+        }
+
+    arregloDinamico.forEach{ println("Valor Actual (it): ${it}")}
+
+    // MAP -> MUTA(Modifica cambia) el arreglo
+    // 1) Enviamos el nuevo valor de la iteracion
+    // 2) Nos devuelve un NUEVO ARREGLO con valores
+    // de las iteraciones
+    val respuestaMap: List<Double> = arregloDinamico
+        .map { valorActual: Int ->
+            return@map valorActual.toDouble() + 100.00
+        }
+    println(respuestaMap)
+    val respuestaMapDos = arregloDinamico.map{ it + 15 }
+    println(respuestaMapDos)
+
+
+
+    // Filter - > Filtrar el ARREGLO
+    // 1) Devolver una expresion (TRUE o FALSE)
+    // 2) Nuevo arreglo FILTRADO
+    val respuestaFilter: List<Int> = arregloDinamico
+        .filter{ valorActual:Int ->
+            // Expresion o CONDICION
+            val mayoresACinco: Boolean = valorActual > 5
+            return@filter mayoresACinco
+        }
+    val respuestaFilterDos =arregloDinamico.filter{ it <=5 }
+    println(respuestaFilter)
+    println(respuestaFilterDos)
+    // OR AND
+    // OR -> ANY (Alguno Cumple?)
+    // And -> ALL (Todos cumplen?)
+    val respuestaAny: Boolean = arregloDinamico
+        .any{ valorActual:Int ->
+            return@any (valorActual > 5)
+        }
+    println(respuestaAny) // True
+    val respuestaAll: Boolean = arregloDinamico
+        .all{ valorActual:Int ->
+            return@all (valorActual > 5)
+        }
+    println(respuestaAll) // False
+
+
+
+
+    // REDUCE -> Valor acumulado
+    // Valor acumulado = 0 (Siempre empieza en 0 en Kotlin)
+    // [1,2,3,4,5] -> Acumular "SUMAR" estos valores del arreglo
+    // valorIteracion1 = valorEmpieza + 1 = 0 +1 = 1 -> Iteracion1
+    // valorIteracion2 = valorAcumuladoIteracion1 + 2 = 1 + 2 = 3 -> Iteracion2
+    // valorIteracion3 = valorAcumuladoIteracion2 + 3 = 3 + 3 = 6 -> Iteracion3
+    // valorIteracion4 = valorAcumuladoIteracion3 + 4 = 6 + 4 = 10 -> Iteracion4
+    // valorIteracion5 = valorAcumuladoIteracion4 + 5 = 10 + 5 = 15 -> Iteracion4
+    val respuestaReduce: Int = arregloDinamico
+        .reduce{ acumulado:Int, valorAcual:Int ->
+            return@reduce (acumulado + valorAcual) // -> Cambiar o usar la logica de negocio
+        }
+    println(respuestaReduce);
 
 }
 fun imprimirNombre(nombre:String): Unit{
@@ -122,6 +211,56 @@ class Suma( // Constructor primario
     dosParametro
 ){
 
+    public val soyPublicoExplicito: String = "Publicas"
+    val soyPublicoImplicite: String = "Publico implicito"
+    init { // bloque constructor primario
+        this.numeroUno
+        this.numeroDos
+        numeroUno // this. OPCIONAL  [propiedades, metodos]
+        numeroDos // this. OPCIONAL  [propiedades, metodos]
+        this.soyPublicoImplicite
+        soyPublicoExplicito
+    }
+    constructor( // Constructor secundario
+        uno: Int?, // Entero nullable
+        dos: Int,
+    ):this(
+        if(uno == null) 0 else uno,
+        dos
+    ){
+        // Bloque de codigo de constructor secundario
+    }
+    constructor( // Constructor secundario
+        uno: Int,
+        dos: Int?, // Entero nullable
+    ):this(
+        uno,
+        if(dos == null) 0 else dos
+    )
+    constructor( // Constructor secundario
+        uno: Int?, // Entero nullable
+        dos: Int?,  // Entero nullable
+    ):this(
+        if(uno == null) 0 else uno,
+        if(dos == null) 0 else dos
+    )
+    fun sumar ():Int{
+        val total = numeroUno + numeroDos
+        agregarHistorial(total)
+        return total
+    }
+    companion object { // Comparte entre todas las instancias, similar al STATIC
+        // funciones, variables
+        // NombreClase.metodo, NombreClase.funcion =>
+        // Suma.pi
+        val pi = 3.14
+        // Suma.elevarAlCuadrado
+        fun elevarAlCuadrado(num:Int):Int{ return num * num }
+        val historialSumas = arrayListOf<Int>()
+        fun agregarHistorial(valorTotalSuma:Int){ // Suma.agregarHistorial
+            historialSumas.add(valorTotalSuma)
+        }
+    }
 }
 
 
